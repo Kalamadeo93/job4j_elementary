760e8b89f5b48745606de24c5e064353f79e3597
Kalamadeo93 <persoonaj@gmail.com>
2023-03-20 20:03:32 +0300

    6. DSL c AssertJ [#504879]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -11,8 +11,8 @@ public class SqArea {
 
     public static void main(String[] args) {
         double result1 = SqArea.square(50, 9);
-        //double result2 = SqArea.square(6, 2);
+        double result2 = SqArea.square(6, 2);
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
-        //System.out.println(" p = 6, k = 2, s = 1, real = " + result2);
+        System.out.println(" p = 6, k = 2, s = 1, real = " + result2);
     }
 }

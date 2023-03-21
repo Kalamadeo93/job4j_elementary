c6f4bb1c1017ace534178ea554431f40a7fa90d0
Kalamadeo93 <persoonaj@gmail.com>
2023-03-21 14:40:06 +0300

    4.2. Тернарное сравнение [#189]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -6,8 +6,15 @@ public class Calculator {
                 + ru.job4j.math.MathFunction.multiply(first, second);
     }
 
+    public static int maxFromThree(int first, int second, int third) {
+        int res = Math.max(first, second);
+        res = Math.max(res, third);
+        return res;
+    }
+
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println(maxFromThree(1, 3, 2));
     }
 }
 

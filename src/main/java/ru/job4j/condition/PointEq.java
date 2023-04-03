fc08026c75353ca2ecb99103d136294b3ea928ee
Kalamadeo93 <persoonaj@gmail.com>
2023-04-03 12:24:20 +0300

    4.3.4. if c return [#333785]

--- src/main/java/ru/job4j/condition/PointEq.java
+++ src/main/java/ru/job4j/condition/PointEq.java
@@ -2,12 +2,7 @@ package ru.job4j.condition;
 
 public class PointEq {
     public static boolean eq(int x1, int y1, int x2, int y2) {
-        /**
-          @deprecated не разобрался, какой должен быть результат работы программы, что конкретно она должна сверить,
-         * факт того, что обе части true или достаточно чтобы true была одна часть, если нужно равенство обеих частей
-         * выражения, знак меняется на &&.
-         */
-        return (x1 == x2) || (y1 == y2);
+        return (x1 == x2) && (y1 == y2);
     }
 
     public static void main(String[] args) {

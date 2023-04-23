3646a339bd6841e57d502b5be9f857337a8bd23a
Kalamadeo93 <persoonaj@gmail.com>
2023-04-23 20:59:25 +0300

    /

--- src/main/java/ru/job4j/loop/LoopFor.java
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -3,10 +3,8 @@ package ru.job4j.loop;
 public class LoopFor {
     public static void main(String[] args) {
         int i;
-        boolean result;
-        for (i = 0; result = i <= 5; i++) {
-            System.out.println(i);
-        }
+        boolean result = false;
+        i = 0;
         System.out.println("Индекс после цикла равен: " + i);
         System.out.println("Условие выполнения цикла равно: " + result);
     }

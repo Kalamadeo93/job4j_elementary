c1df426891b5c83d568e9b621d33b83550c33caa
Kalamadeo93 <persoonaj@gmail.com>
2025-09-25 17:49:19 +0300

    /

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,11 +2,6 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        poe();
-    }
-    public static void poe() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
+        System.out.println("Hello World!");
     }
 }

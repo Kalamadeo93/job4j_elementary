fc8e523a8d9e511bcd3017a20550ee8cc067c711
Kalamadeo93 <persoonaj@gmail.com>
2025-09-25 17:54:04 +0300

    1. Чтение кода и базовые ошибки [#40205 #552392]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -2,11 +2,9 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args) {
-        {
-            int[] numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
-            for(int i: numbers){
-                System.out.println("1 * "+ (i)+ " = " + (i));
-            }
+        int[] numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i: numbers) {
+                System.out.println("1 * " + (i) + " = " + (i));
         }
     }
 }
